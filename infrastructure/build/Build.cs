17b55f4c9978c25334f29eeeb8f3bf002eb8a1c2
Andrej Matijevic <andrej.matijevic@gmail.com>
2023-11-07 07:15:37 +0100

    feat: Add comment (#1)

--- infrastructure/build/Build.cs
+++ infrastructure/build/Build.cs
@@ -24,6 +24,7 @@ class Build : NukeBuild
 {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
