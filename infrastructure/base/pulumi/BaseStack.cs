956c8ec1ea8ab3a2dbd6fc2463bfb0d28934a233
AMatijevic <andrej.matijevic@gmail.com>
2023-12-05 07:28:33 +0100

    #3 feat: Add pulumi project that is currently only adding one ResourceGroup and test managed user identity

---
+++ infrastructure/base/pulumi/BaseStack.cs
@@ -0,0 +1,51 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+using Azure = Pulumi.AzureNative;
+public class BaseStack : Stack
+{
+    private const string Name = "cistilec";
+
+    private readonly StackConfiguration _configuration;
+    public BaseStack()
+    {
+        _configuration = new StackConfiguration();
+
+        var resourceGroup = CreateResourceGroup($"rg-{Name}-{_configuration.Environment}");
+
+        //User Assigned Managed Identity
+        var userAssignedIdentity = new Azure.ManagedIdentity.UserAssignedIdentity(
+            "myUserAssignedIdentity", 
+            new Azure.ManagedIdentity.UserAssignedIdentityArgs
+            {
+                ResourceGroupName = resourceGroup.Name,
+                Location = _configuration.Region,
+                Tags = _configuration.Tags,
+            });
+        var principalId = userAssignedIdentity.PrincipalId;
+
+        var exampleAssignment = new Azure.Authorization.RoleAssignment(
+            "exampleAssignment", 
+            new()
+            {
+                Scope = "/subscriptions/d0f36a26-d10f-4b60-8bc9-05b8087c19b2",
+                RoleDefinitionId = "/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c",
+                PrincipalId = principalId//.Apply(getClientConfigResult => getClientConfigResult),
+            });
+
+    }
+
+    private ResourceGroup CreateResourceGroup(string resourceGroupName)
+    {
+        return new ResourceGroup(
+            resourceGroupName,
+            new ResourceGroupArgs
+            {
+                ResourceGroupName = resourceGroupName,
+                Location = _configuration.Region,
+                Tags = _configuration.Tags
+            });
+    }
+}
---
+++ infrastructure/base/pulumi/StackConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Pulumi;
+using System.Collections.Generic;
+
+/// <summary>
+/// [Configuration | Pulumi Concepts | Pulumi Docs](https://www.pulumi.com/docs/concepts/config/)
+/// </summary>
+public class StackConfiguration
+{
+    private Dictionary<string, string> _tags = [];
+
+    protected readonly Config StackConfig;
+
+    protected readonly Config AzureStackConfig;
+
+    public string Environment => StackConfig.Require("environment");
+
+    public string Region => AzureStackConfig.Require("region");
+
+    public Dictionary<string, string> Tags
+    {
+        get
+        {
+            if (_tags.Count != 0) { return _tags; }
+            
+            _tags = StackConfig.RequireObject<Dictionary<string, string>>("tags");
+            _tags.Add("environment", Environment);
+            _tags.Add("createdBy", "Pulumi");
+            
+            return _tags;
+        }
+    }
+    public StackConfiguration()
+    {
+        StackConfig = new Config();
+        AzureStackConfig = new Config("azure-native");
+    }
+
+    public bool IsEnvironment(string environment)
+    {
+        return Environment == environment;
+    }
+}
